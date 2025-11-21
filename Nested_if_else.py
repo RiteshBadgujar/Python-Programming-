0ab26ca4a8d7d76467ed4027f026a0f8d84d5d98
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-11-21 22:57:30 +0530

    [ADDED]:Ritesh - The nested If-Else Statement .

---
+++ Nested_if_else.py
@@ -0,0 +1,10 @@
+age=int(input("Enetr your age :"))
+
+#nesting 
+if(age >=18):
+    if(age>= 90):
+        print("Cannot drive...!")
+    else:
+        print("can drive.")
+else:
+    print("cannot drive...!")
