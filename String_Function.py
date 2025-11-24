5ab74c150c13ad6bca9c59a0c2d53646932d0fe3
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-11-24 22:02:04 +0530

    [ADDED]:Ritesh -:The String function.

---
+++ String_Function.py
@@ -0,0 +1,24 @@
+str="be Positive ,all time"
+print(str.endswith('ve'))
+print(str.endswith("re"))
+
+print(str.capitalize())
+print(str)
+str=str.capitalize()
+print(str)
+
+print(str.replace("e","o"))
+print(str.replace("Be","All Time"))
+
+print(str.find("i"))
+print(str.find("all"))
+print(str.find("Q"))
+
+print(str.count("i"))
+print(str.count("all "))
+
+name=input("Enter the Your name:")
+print("Lenght of your name:",len(name))
+
+str1="Hi am Rk $ is $ $"
+print(str1.count("$"))
