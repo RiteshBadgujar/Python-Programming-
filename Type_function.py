b7aacfbdfb4dd405e9845b93ef7889a41eab8adb
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-11-25 22:49:56 +0530

    [ADDED]:Ritesh -:The Dynamic Input with Type Checking Program.

---
+++ Type_function.py
@@ -0,0 +1,9 @@
+name = input("Enter your name :")
+print(name,type(name))
+
+no = int(input("Enter the number :"))
+print(no,type(no))
+
+mark =float(input("Enter your mark :"))
+print(mark,type(mark))
+
