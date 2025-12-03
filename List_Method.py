6622c7a13a08c67e0587abda7909abdfb9ed257c
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-03 23:51:14 +0530

    [ADDED]:Ritesh -:The python list method program.

---
+++ List_Method.py
@@ -0,0 +1,21 @@
+mark = [2,3,1,5,6]
+mark.append(4) # Add element at tha last.
+print(mark)
+
+mark.reverse() #Reverse list
+print(mark)
+
+mark.sort() #Sort in assending order
+print(mark)
+
+mark.sort(reverse=True) #Sort in descending order
+print(mark)
+
+mark.insert(2,7) #Insert element in index
+print(mark)
+
+mark.remove(6) #Remove first occurence of element
+print(mark)
+
+mark.pop(2) #remove element at index
+print(mark)
