057873675bbaa3ab5b71d4a808a6b8036a8deed7
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-04 22:52:49 +0530

    [ADDED]:Ritesh -:The Loop using range method program.

---
+++ Range_Function.py
@@ -0,0 +1,8 @@
+for i in range(7):  #range(start,stop,step size)
+    print(i)
+
+for i in range(3,8): #range(start,stop)
+    print(i)
+    
+for i in range(2,7,2):  #range(start,stop,step size)
+    print(i)
