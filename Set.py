e583ef7013b2f2be6728f29b354f6774d506839e
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-05 00:05:46 +0530

    [ADDED]:Ritesh -:The python set program example.

---
+++ Set.py
@@ -0,0 +1,6 @@
+nums={1,2,2,3,"Hello","Hello",4}  #Collection of unorder items.unique & immutable
+print(nums)
+
+print(len(nums))    #check element length.
+
+collection =set()   #Empty set created. 
