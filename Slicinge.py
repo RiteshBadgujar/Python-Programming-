f431963b630807a2efea718b99f28b582f6e5b8f
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-04 22:50:44 +0530

    [ADDED]:Ritesh -:Added Examples for Python list slicing.

---
+++ Slicinge.py
@@ -0,0 +1,14 @@
+str="Ritesh Badgujar" 
+
+print(str[1:4])
+print(str[0 :7 ])
+
+print(str[10:len(str)])
+print(str[:len(str)])  
+print(str[0:])
+
+#Negetive Index
+str2="Change Mindset"
+
+print(str2[-3:-1])
+print(str2[:-1])
