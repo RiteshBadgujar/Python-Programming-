fc132e0e826e640f6e2c1e7f3942c30dc024f010
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-05 00:07:14 +0530

    [ADDED]:Ritesh -:The python set method example program.

---
+++ Set_Method.py
@@ -0,0 +1,16 @@
+nums={"Collage",1,2,"Ms D",2,3,"Hello","Hello",4}  #Collection of unorder items.unique & immutable
+print(nums)
+
+num2={1,2,"Hello","Ritesh",3,5}
+print(len(nums))    #check element length.
+collection =set()   #Empty set created. 
+
+print(nums.pop())
+print(nums.pop())
+
+print(nums.union(num2))
+print(nums.intersection(num2))
+
+
+
+
