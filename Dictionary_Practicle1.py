01f4e3e7bad9c8da75b942b693cf278c8f570344
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-06 00:29:15 +0530

    [ADDED]:Ritesh -:The python dictionary method practicle program.

---
+++ Dictionary_Practicle1.py
@@ -0,0 +1,18 @@
+dict={
+    "table":["a piace of furniture,"]+["list of fact & figure"],
+    "cat":"a small animal"
+}
+print(dict)
+
+set={"python","java","c++","python",
+     "javascript","java","python",
+     "java","c++","c"
+     }
+print(set,len(set))
+
+sub={
+    "bio":input("Enter mark in bio :"),
+    "math":input("Enter the math :"),
+    "chem":input("Enetr the chem :")
+}
+print(sub)
