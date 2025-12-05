c4a7b35fb65f08b337d669c42b4a7dd3201af985
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-05 16:21:50 +0530

    [ADDED]:Ritesh -:The python dictionary example program.

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+dict ={
+    "name" : "Ritesh",
+    "Age" : 22,
+    "Class":"Mca"
+}
+print(dict)
+#Store tuple and list to dictionary
+dict2={
+   "name" :["Ritesh","Shubham","Jayesh"],
+   "Age" :(22,33,23),
+   "Location":"Nashik"
+}
+print(dict2,"\n",type(dict2))
