36813a7b95983d1e6421cb8ac0d9940b2d337116
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-07 00:22:27 +0530

    [ADDED]:Ritesh -:The list & tuple practicle program.

---
+++ List&Tuple_Practicle.py
@@ -0,0 +1,8 @@
+movies1=input("Enter the 1 movies Name:")
+movies2=input("Enter the 2 Movies Name:")
+movies3=input("Enter the 3 Movies Name :")
+
+list=[movies1]+[movies2]+[movies3]
+print(list)
+
+
