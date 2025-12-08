69f13a0ce263a37e7b346841adc5a96ead3ad823
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-08 21:01:06 +0530

    [ADDED]:Ritesh -:The range function practicle program.

---
+++ Range_Function_p1.py
@@ -0,0 +1,12 @@
+# #print 1 to 100
+for i in range(1,101):
+    print(i)
+
+#print 100 to 1
+for j in range(101,0,-1):
+    print(j)
+
+#Print a table any no :
+var=int(input("Enter the no :"))
+for i in range(1,11):
+    print(i*var)
