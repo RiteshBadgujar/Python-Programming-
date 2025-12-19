484c561db02ceb59f07b97fbdc835bb1c357d3be
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-19 19:18:10 +0530

    [ADDED]:Ritesh -:The function practicle question 2 program .

---
+++ Function_practicle2.py
@@ -0,0 +1,12 @@
+# Wap to print element of a list in a sinle line.(List is the parameter)
+
+cities = ["pune","Mumbai","Nashik","Delhi"]
+
+def print_list(list):
+    for item in list:
+        print(item,end=" ")
+    
+print_list(cities)
+
+
+    
