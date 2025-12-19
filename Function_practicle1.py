c7397ed37a2233642bf29975483dbd12020aa50f
RiteshBadgujar <riteshbadgujar0@gmail.com>
2025-12-19 19:17:32 +0530

    [ADDED]:Ritesh -:The function practicle question program .

---
+++ Function_practicle1.py
@@ -0,0 +1,12 @@
+# Wap to print the length of list.(List is the parameter)
+
+cities = ["pune","Mumbai","Nashik","Delhi"]
+no = [1,2,3,4,5,6]
+
+def length(list):
+   print("List length is :",len(list))
+
+length(cities)
+length(no)
+
+    
